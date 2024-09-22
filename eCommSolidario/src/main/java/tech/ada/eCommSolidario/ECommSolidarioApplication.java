49f9307b59951cecd754fe51b2be7c2dc9fb1dc2
Rogerio Meira <raazeved@hotmail.com>
2024-09-22 16:49:18 -0300

    Atualiza Projeto
    Atualiza matriz do projeto

---
+++ eCommSolidario/src/main/java/tech/ada/eCommSolidario/ECommSolidarioApplication.java
@@ -0,0 +1,13 @@
+package tech.ada.eCommSolidario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommSolidarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommSolidarioApplication.class, args);
+	}
+
+}
---
+++ eCommSolidario/src/test/java/tech/ada/eCommSolidario/ECommSolidarioApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ada.eCommSolidario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommSolidarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
