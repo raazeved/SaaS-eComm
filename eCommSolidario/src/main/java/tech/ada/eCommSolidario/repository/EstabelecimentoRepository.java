ffbfbf99e21fb841f7711f3b29c8910b189a4a1a
GrossiJr <fgrossijr@hotmail.com>
2024-09-28 11:47:13 -0300

    Adicionando meu arquivos para Estabelecimento

---
+++ eCommSolidario/src/main/java/tech/ada/eCommSolidario/repository/EstabelecimentoRepository.java
@@ -0,0 +1,7 @@
+package tech.ada.eCommSolidario.repository;
+
+import tech.ada.eCommSolidario.model.Estabelecimento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento, Long> {
+}
